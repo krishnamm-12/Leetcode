a3a396560657f16f02b14418a7fdead1c4bb1432
krishnamm-12 <gkrishnam668@gmail.com>
2026-08-18 18:35:23 +0530

    Time: 49 ms (74.75%) | Memory: 18.8 MB (39.58%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& m1, int target) {
+        int n=m1.size();
+        int m=m1[0].size();
+      
+        int row=0,col=m-1;
+        while(row<n && col>=0){
+            if(m1[row][col]==target) return true;
+            else if(m1[row][col]<target) row++;
+            else if(m1[row][col]>target) col--;
+
+        }
+        return false;
+    }
+};
