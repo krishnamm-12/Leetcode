02384d2d96945c4742c9066e24fc764dda44e03c
krishnamm-12 <gkrishnam668@gmail.com>
2026-08-21 01:30:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (82.93%) - LeetSync

---
+++ 4177-reverse-string-prefix/reverse-string-prefix.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string reversePrefix(string s, int k) {
+        int i=0,j=k-1;
+        while(i<j){
+            swap(s[i],s[j]);
+            i++;
+            j--;
+        }
+        return s;
+    }
+};
